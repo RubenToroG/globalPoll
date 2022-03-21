a873ada9615956553a536753382625468bf01188
Ruben Toro <rubensdt01@gmail.com>
2022-03-21 00:15:42 -0500

    Se agrega vista de resultados de la encuesta

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render, get_list_or_404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
@@ -20,8 +20,10 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f'Estás viendo los resultados de la pregunta número {question_id}')
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        'question': question
+    })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
