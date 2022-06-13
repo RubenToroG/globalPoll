f101edf2f83e7e3d4d215b77338ac058e76f1b57
Ruben Toro <rubensdt01@gmail.com>
2022-06-13 10:51:16 -0500

    test from was_published_recently

--- premiosapp/polls/tests.py
+++ premiosapp/polls/tests.py
@@ -1,3 +1,14 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTest(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently return false for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text='¿Cuál es la mejor compañia del mundo?', pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
